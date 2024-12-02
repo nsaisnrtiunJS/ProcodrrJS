30b33c388e85c6b381f715b676e57c1598456915
SamCodrr <161819371+nsaisnrtiunJS@users.noreply.github.com>
2024-12-02 09:28:52 +0000

    codeExecution

---
+++ procodrrJS/01_datatypes.js
@@ -0,0 +1,25 @@
+// There are 2 types of data types. These are 1.Primitive and 2.Non-Primitive
+
+/* 1.Primitive:-
+          7types:- 1.Number,2.String,3.Boolean,4.Undefined,5.Null,6.bigInt and 7.Symbol
+          */
+
+
+/*   2. Non-Primitive:- 
+             3types:--- 1.Array, 2.Object and 3.Function       
+             */
+
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// debugger
+var firstName = "Sam";
+let lastName = "Islam";
+let myAge = 25;
+const yearOfBirth = 1999;
+
+let userIntro = "Hello my name is " + firstName + " " + lastName + ". I am " + myAge + ". My year of birth is " + yearOfBirth;
+
+console.log(userIntro);
