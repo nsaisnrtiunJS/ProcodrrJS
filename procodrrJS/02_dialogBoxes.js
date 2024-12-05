d0f67a164b32d8234f642694502624dd4e19683a
SamCodrr <161819371+nsaisnrtiunJS@users.noreply.github.com>
2024-12-05 14:15:17 +0000

    dialogBoxes

---
+++ procodrrJS/02_dialogBoxes.js
@@ -0,0 +1,11 @@
+// DIALOG BOXES
+
+/*There are three types of Dialog boxes. Here are all :-
+
+01. Alert("your message goes here")
+02. Confirm("your message goes here")
+03. Prompt("your message goes here")
+*/
+
+const isConfirm = confirm("Are you student?");
+console.log(isConfirm)
