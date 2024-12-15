2de12db06d39c5ded610c4128b9c012fc30a1d96
SamCodrr <161819371+nsaisnrtiunJS@users.noreply.github.com>
2024-12-15 11:36:56 +0000

    comparisonOperators

---
+++ procodrrJS/06_comparisonOperators.js
@@ -0,0 +1,38 @@
+//+++++++++++++++++           COMPARISON OPERATORS       +++++++++
+/****Comparison Operators are used in logical statement to determine equality or compare between variables or values */
+/*                      OPERATORS:-
+== (equal to)
+=== (equal value and equal type)
+!= (Not equal)
+!== (Not equal value or not equal type)
+> (greater than)
+< (less then)
+>= (greater than or equal)
+<= (less than or equal)
+      */
+
+const a = 5
+const b = 8
+const c = '13'
+
+console.log(a==b)
+console.log(a+b == c)
+
+console.log(a===b)
+console.log(a+b ===c)
+
+const x = 10
+const y = '10'
+const z = 7
+console.log(x != y)
+console.log(x !== y)
+
+console.log(x > y)
+console.log(z < y)
+console.log(x >= y)
+console.log(z <= y) 
+
+
+const message = "Hello"
+const message1 = 'Hello'
+
