ea1aca422f1e857f3ea1ad586d38672b1d880479
SamCodrr <161819371+nsaisnrtiunJS@users.noreply.github.com>
2024-12-19 16:40:15 +0000

    logical Operators

--- procodrrJS/08_logicalOperators.js
+++ procodrrJS/08_logicalOperators.js
@@ -1,18 +1,30 @@
 /* Logical Operators are used to combine two or more conditions together and return a single true or false value    */
 
+
+
+
+
 //********+++++++++++++++++++++++++++++++++++++&&&&&&&&++++++++++++++
 /*  01. && (And Operator)
     02. || (Or Operator)
     03. !  (Not Operator)
     */
 
+
+
+
     const name = 'Samiul'
     const lastName = 'Islam'
-    const age = 25;
-    const lastTwoDigit = 20
-    // console.log(name && age)
-    // console.log(age && name)
-    // console.log(name && lastName)
-    // console.log(lastName && name)
-    console.log(age && lastTwoDigit)
-    
+    const userAge = '25'
+    // const lastTwoDigit = 20
+  
+
+const isChild = (userAge >= 0 && userAge <= 5)
+const isSchoolStudent = (userAge > 5 && userAge <= 18) 
+const isCollegeStudent = (userAge > 18 && userAge <= 24)
+const isWorkingProfessional = (userAge > 24 && userAge <= 55)
+
+console.log(isChild)
+console.log(isSchoolStudent)
+console.log(isCollegeStudent)
+console.log(isWorkingProfessional)
